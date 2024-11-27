d83d16a854961b00107200e4ff6a2e1f774857d4
Bruno <brunoo121mendes@gmail.com>
2024-11-27 20:14:08 -0300

    script.js

--- projeto buffet/JavaScript/script.js
+++ projeto buffet/JavaScript/script.js
@@ -3,16 +3,17 @@ function calcularOrcamento() {
     numConvidados = document.getElementById('convidados').value;
     evento = document.querySelector('input[name="eventType"]:checked').value;
     cardapio = document.querySelector('input[name="menuType"]:checked').value;
-
+    
     precoPorPessoa = 0;
     if (cardapio == 'Churrasco') {
-        precoPorPessoa = 50; 
-    } else if (cardapio == 'Coquetel Premium') {
-        precoPorPessoa = 70;
+        precoPorPessoa = 125; 
+    } else if (cardapio == 'Feijoada') {
+        precoPorPessoa = 105;
     } else if (cardapio == 'Festival de Massas') {
-        precoPorPessoa = 40;
+        precoPorPessoa = 95;
     }
     custoTotal = precoPorPessoa * numConvidados;
 
-    document.getElementById('orcamento').textContent = ` R$ ${custoTotal}`;
+    document.getElementById('orcamento').textContent = ` R$ ${custoTotal
+    }`;
 }
